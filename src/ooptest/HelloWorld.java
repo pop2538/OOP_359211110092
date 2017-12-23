f459d0d41c96251402483ff999a2a63ec9c2521d
pop2538 <prutsorn2281@hotmail.com>
2017-12-23 09:00:54 -0800

    Testing IS Data

---
+++ src/ooptest/HelloWorld.java
@@ -0,0 +1,12 @@
+package ooptest;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world,I'm pop");
+        //  Add Data
+        System.out.println("prutsorn");
+        System.out.println("Information System");
+        System.out.println("Faculty of Management Technology,RMUTSV");
+
+    }//main
+}//class
