0b0e571e30a94e81c0aede3275d004753f1e889a
pop2538 <prutsorn2281@hotmail.com>
2018-01-24 09:21:17 +0700

    Class and object Student Class

---
+++ src/ooptest7/Student.java
@@ -0,0 +1,70 @@
+package ooptest7;
+//talking about Student?
+//1.Student ID
+//2.Name
+//3.Address
+//4.Gender
+
+import com.sun.org.apache.xpath.internal.operations.String;
+
+public class Student {
+    //properties of Student
+    private String id;
+    private String name;
+    private String address;
+    private String gender;
+    //Constructor
+    //Defult Constructor
+    public Student () {}
+    //Constructor by own
+    public  Student (String id,String n, String a, String g) {
+        //assign data to properties
+        this.id = id;
+        this.name = n;
+        this.address = a;
+        this.gender = g;
+    }//Student
+    //getter and setter methods
+
+    @Override
+    public java.lang.String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name=" + name +
+                ", address=" + address +
+                ", gender=" + gender +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}//class
