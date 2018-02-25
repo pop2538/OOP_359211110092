d8d6e4608f7d1b5d307bec3faccd68aa294ef194
game1995 <game_130@hotmail.com>
2018-02-25 15:50:30 +0700

    CatPet

---
+++ src/ooplab8/CatApp.java
@@ -0,0 +1,9 @@
+package ooplab8;
+
+public class CatApp {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Yaya",1);
+        System.out.println(cat.toString());
+        cat.makeNoise();
+    }
+}//class
