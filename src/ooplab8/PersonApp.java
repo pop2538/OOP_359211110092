73691d2f2b21f04cb25bd081c5451b28cedbcbd6
game1995 <game_130@hotmail.com>
2018-02-25 15:56:27 +0700

    Inheritance:Employee

---
+++ src/ooplab8/PersonApp.java
@@ -0,0 +1,10 @@
+package ooplab8;
+
+public class PersonApp {
+    public static void main(String[] args) {
+        Student student = new Student("Boy Saiya", "Male", "20/12/1995",
+                20,"STD001","IS:MT");
+        System.out.println(student.toString());
+
+    }//main
+}//class
