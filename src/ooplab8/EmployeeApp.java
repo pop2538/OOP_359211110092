24913ab1c68d0902d6febb66219a1814fd835a56
game1995 <game_130@hotmail.com>
2018-02-25 15:54:24 +0700

    Inheritance:Employee

---
+++ src/ooplab8/EmployeeApp.java
@@ -0,0 +1,9 @@
+package ooplab8;
+
+public class EmployeeApp {
+    public static void main(String[] args) {
+        Employee employee = new Employee("KIM", "Male", "20/02/1996",
+                20,"TT","15000");
+        System.out.println(employee.toString());
+    }//main
+}//class
